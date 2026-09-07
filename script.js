1d6a0805dca960ddfa7c4374bcb9addf0743bfa8
randombelo <abelmarrerocamero@gmail.com>
2026-09-07 13:30:09 +0200

    feat: solving strings exercises

--- script.js
+++ script.js
@@ -41,4 +41,50 @@ function Random(){
     const lol =new Date().getSeconds()
     return ((1/Date.now())**(1/12))*(1/lol)
 }
-
+//Letras 
+//Ex1
+function Concat(cad1,cad2){
+    return cad1+cad2
+}
+//Ex2
+function Long(cad1){
+    return cad1.length
+}
+//Ex3
+function Upper(cad1){
+    return cad1.toUpperCase()
+}
+//Ex4
+function Lower(cad1){
+    return cad1.toLowerCase()
+}
+//Ex5
+function Index(cad1,x){
+    return cad1[x]
+}
+//Ex6
+function Reverse(cad1){
+    return cad1.split("").reverse().join("")
+}
+//Ex7
+function Search(cad1,car1){
+    let count=0
+    for(x of cad1){
+        if(x===car1){
+            count++
+        }
+    }
+    return count
+}
+//Ex8
+function withoutBlank(cad1){
+    return cad1.split(" ").join("")
+}
+//Ex9
+function IsPalimdrome(cad1){
+    return cad1===Reverse(cad1)
+}
+//Ex10
+function UpperPhrase(cad1){
+    return cad1.split(" ").map(p => p[0].toUpperCase() + p.slice(1)).join(" ");
+}
